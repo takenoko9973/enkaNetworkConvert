59747a7c61576218cc43740f58ef9e6f684cd112
taketomo3141 <sapphire3141@gmail.com>
2022-08-30 05:36:13 +0900

    翻訳classを追加

--- Enka.Network_lang-jp.user.js
+++ Enka.Network_lang-jp.user.js
@@ -7,27 +7,42 @@
     const $charaStats = $doc.getElementsByClassName("StatsTable");
     const $artifact = $doc.getElementsByClassName("Artifact");
 
-    const LANGUAGE = {
-        EN: "EN",
-        JA: "JA",
-    };
+    const converterInstance = new EnkaConverter();
 
-    function getConvertJsonURL(language) {
-        switch (language) {
-            case LANGUAGE.EN:
-                return "https://raw.githubusercontent.com/takenoko9973/enkaNetworkConvert/master/en.json";
-            case LANGUAGE.JA:
-                return "https://raw.githubusercontent.com/takenoko9973/enkaNetworkConvert/master/ja.json";
-            default:
-                return "https://raw.githubusercontent.com/takenoko9973/enkaNetworkConvert/master/en.json";
-        }
+    function getLanguage() {
+        const $language = $doc.getElementsByXPath('//div[@data-icon="language"]')[0];
+        return $language.innerText;
     }
 
-    fetch(getConvertJsonURL("JA"))
-        .then(response => {
-            return response.json();
-        })
-        .then(jsondata => console.log(jsondata));
+    function getConvertStatName(key, isSub) {
+        const language = getLanguage();
+        const name = converterInstance.getStatName(language, key, isSub);
+
+        return name;
+    }
+
+    /**
+     * 表示させているプレイヤーのUIDと名前を取得
+     */
+    function getPlayerInfo() {
+        const playerUID = location.pathname.split("/")[2]; // urlからUIDを取得
+        const $playerInfo = $doc.getElementsByClassName("PlayerInfo")[0];
+        const playerName = $playerInfo.getElementsByTagName("h1")[0].innerText; // プレイヤー名を取得
+
+        return [playerUID, playerName];
+    }
+
+    /**
+     * キャラクターの合計ステータスを取得
+     */
+    function getCharacterStats(key) {
+        let index = -1;
+        const $statsList = $charaStats[0].children;
+        if ((index = Array.from($statsList).map(stat => stat.classList[1]).indexOf(key)) === -1) return 0;
+
+        const stat = $statsList[index].children[1].children[2].innerText;
+        return Number(stat.replace(/[^0-9.]/g, ''));
+    }
 
     window.onload = function () {
     };
---
+++ enkaConverter.js
@@ -0,0 +1,132 @@
+const LANGUAGE = {
+    EN: "EN",
+    JA: "JA",
+};
+
+class EnkaConverter {
+    constructor() {}
+
+    CONVERT_TEXT = {
+        BASE_ATK: {
+            key: "BASE_ATK",
+            [LANGUAGE.EN]: "Base ATK",
+            [LANGUAGE.JA]: "基礎攻撃力",
+        },
+        HP: {
+            key: "HP",
+            [LANGUAGE.EN]: "HP",
+            [LANGUAGE.JA]: "HP",
+        },
+        ATK: {
+            key: "ATTACK",
+            [LANGUAGE.EN]: "ATK",
+            [LANGUAGE.JA]: "攻撃力",
+        },
+        DEF: {
+            key: "DEFENSE",
+            [LANGUAGE.EN]: "DEF",
+            [LANGUAGE.JA]: "防御力",
+        },
+        CRIT_RATE: {
+            key: "CRITICAL",
+            [LANGUAGE.EN]: "CRIT Rate",
+            [LANGUAGE.JA]: "会心率",
+        },
+        CRIT_DMG: {
+            key: "CRITICAL_HURT",
+            [LANGUAGE.EN]: "CRIT DMG",
+            [LANGUAGE.JA]: "会心ダメージ",
+            sub: {
+                [LANGUAGE.JA]: "会心ダメ",
+            }
+        },
+        EM: {
+            key: "ELEMENT_MASTERY",
+            [LANGUAGE.EN]: "Elemental Mastery",
+            [LANGUAGE.JA]: "元素熟知",
+            sub: {
+                [LANGUAGE.EN]: "EM",
+            }
+        },
+        ENERGY_RECHARGE: {
+            key: "CHARGE_EFFICIENCY",
+            [LANGUAGE.EN]: "Energy Recharge",
+            [LANGUAGE.JA]: "元素チャージ",
+            sub: {
+                [LANGUAGE.EN]: "ER",
+                [LANGUAGE.JA]: "元素チャ",
+            }
+        },
+        CRYO: {
+            key: "ICE_ADD_HURT",
+            [LANGUAGE.EN]: "Cryo DMG",
+            [LANGUAGE.JA]: "氷元素ダメージ",
+        },
+        ANEMO: {
+            key: "WIND_ADD_HURT",
+            [LANGUAGE.EN]: "Anemo DMG",
+            [LANGUAGE.JA]: "風元素ダメージ",
+        },
+        ELECTRO: {
+            key: "ELEC_ADD_HURT",
+            [LANGUAGE.EN]: "Electro DMG",
+            [LANGUAGE.JA]: "雷元素ダメージ",
+        },
+        HYDRO: {
+            key: "WATER_ADD_HURT",
+            [LANGUAGE.EN]: "Hydro DMG",
+            [LANGUAGE.JA]: "水元素ダメージ",
+        },
+        PYRO: {
+            key: "FIRE_ADD_HURT",
+            [LANGUAGE.EN]: "Pyro DMG",
+            [LANGUAGE.JA]: "炎元素ダメージ",
+        },
+        GEO: {
+            key: "ROCK_ADD_HURT",
+            [LANGUAGE.EN]: "Geo DMG",
+            [LANGUAGE.JA]: "岩元素ダメージ",
+        },
+        PHYS: {
+            key: "PHYSICAL_ADD_HURT",
+            [LANGUAGE.EN]: "Physical DMG",
+            [LANGUAGE.JA]: "物理ダメージ",
+        },
+        HEAL_BNS: {
+            key: "HEAL_ADD",
+            [LANGUAGE.EN]: "Healing Bonus",
+            [LANGUAGE.JA]: "与える治癒効果",
+        },
+        FRIEND: {
+            key: "FRIEND",
+            [LANGUAGE.EN]: "Friendship",
+            [LANGUAGE.JA]: "好感度",
+        },
+        UNKNOWN: {
+            key: "UNKNOWN",
+            [LANGUAGE.EN]: "Unknown",
+            [LANGUAGE.JA]: "不明",
+        }
+    }
+
+    getStatByClassName(className) {
+        var array = Object.keys(CONVERT_TEXT).map((k)=>(CONVERT_TEXT[k]));
+
+        return array.find((s) => s.key === className);
+    }
+
+    getStatName(language, key, isSub) {
+        // 対応していない言語ならば、英語に強制的に変更
+        if (!(language in LANGUAGE)) language = LANGUAGE.EN;
+
+        let stat = getStatByClassName(key);
+        if (!stat) return CONVERT_TEXT.UNKNOWN[language];
+        if (!isSub) return stat[language];
+
+        // サブステータス時の動作
+        if (!("sub" in stat)) return stat[language];
+        if (!(language in stat["sub"])) return stat[language];
+
+        return stat["sub"][language];
+    }
+}
