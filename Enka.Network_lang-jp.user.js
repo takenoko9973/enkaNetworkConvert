17d8ae8f3c303b3abfb213faca739f521a75b5cc
Takenoko-ya <62060755+takenoko9973@users.noreply.github.com>
2022-08-30 12:45:58 +0900

    Update Enka.Network_lang-jp.user.js

--- Enka.Network_lang-jp.user.js
+++ Enka.Network_lang-jp.user.js
@@ -10,7 +10,7 @@
 // @require      https://github.com/takenoko9973/enkaNetworkConvert/raw/master/main.js
 // @require      https://github.com/takenoko9973/enkaNetworkConvert/raw/master/enkaConverter.js
 // @require      https://github.com/takenoko9973/enkaNetworkConvert/raw/master/util.js
-// @since        0.40T enka.netoworkのアップデートに対応
+// @since        0.40T enka.networkのアップデートに対応
 // @since        0.31T 聖遺物が5か所 または サブOPが4か所埋まっていないときに正常に表示されない不具合を修正 主人公を選択したとき、好感度部分でエラーが発生する不具合修正
 // @since        0.30T 英語表示対応
 // @since        0.21T URLの修正
