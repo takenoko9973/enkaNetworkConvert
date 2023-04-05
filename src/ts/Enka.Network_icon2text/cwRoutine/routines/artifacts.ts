8840bae07918053c29a536b2e090942f9a0597d4
taketomo3141 <sapphire3141@gmail.com>
2023-04-05 21:50:53 +0900

    artifactのroutineのクラス名を変更

--- src/ts/Enka.Network_icon2text/cwRoutine/routines/artifacts.ts
+++ src/ts/Enka.Network_icon2text/cwRoutine/routines/artifacts.ts
@@ -1,16 +1,15 @@
 import { cssManager } from "../../myConst";
 import { addStatTextElement, getSvelteClassName } from "../../util/enkaUtil";
-import { fmt } from "../../util/fmt";
 import { innerOptionText } from "../../util/innerOptionText";
 import { CreateWriteRoutine } from "../createWriteRoutine";
 
-export class Artifact implements CreateWriteRoutine {
-    private static _instance: Artifact;
+export class ArtifactRoutine implements CreateWriteRoutine {
+    private static _instance: ArtifactRoutine;
     private artifacts = document.getElementsByClassName("Artifact");
 
-    public static get instance(): Artifact {
+    public static get instance(): ArtifactRoutine {
         if (!this._instance) {
-            this._instance = new Artifact();
+            this._instance = new ArtifactRoutine();
         }
 
         return this._instance;
@@ -33,12 +32,12 @@ export class Artifact implements CreateWriteRoutine {
 
         const svelte = getSvelteClassName(this.artifacts[0]);
         const cssStyle = [
-            ".Artifact.${svelte} .ArtifactIcon { top: -37%; left: -6%; width: 28%; }", // 聖遺物画像の調整
-            ".substats.${svelte} > .Substat { display: flex; align-items: center; padding-right: 1.0em; white-space: nowrap; }", // 聖遺物のサブステータスが右に行きすぎるので調整
-            ".mainstat.${svelte} > div.${svelte}:nth-child(1) { display: flex; align-items: center; top: 5%; line-height:0.9; max-height: 25%; text-shadow: rgba(0,0,0,0.2) 2px 2px 1px; font-weight:bold; justify-content: flex-end; align-self: unset; margin-left: unset;}", // 聖遺物メインステータスの調整
-            ".mainstat.${svelte} > div.${svelte}:nth-child(2) { padding: 4% 0%; }",
-            ".mainstat.${svelte} > div.${svelte}:nth-child(3) { max-height: 25% }",
-        ].map(css => fmt(css, { svelte: svelte }));
+            `.Artifact.${svelte} .ArtifactIcon { top: -37%; left: -6%; width: 28%; }`, // 聖遺物画像の調整
+            `.substats.${svelte} > .Substat { display: flex; align-items: center; padding-right: 1.0em; white-space: nowrap; }`, // 聖遺物のサブステータスが右に行きすぎるので調整
+            `.mainstat.${svelte} > div.${svelte}:nth-child(1) { display: flex; align-items: center; top: 5%; line-height:0.9; max-height: 25%; text-shadow: rgba(0,0,0,0.2) 2px 2px 1px; font-weight:bold; justify-content: flex-end; align-self: unset; margin-left: unset;}`, // 聖遺物メインステータスの調整
+            `.mainstat.${svelte} > div.${svelte}:nth-child(2) { padding: 4% 0%; }`,
+            `.mainstat.${svelte} > div.${svelte}:nth-child(3) { max-height: 25% }`,
+        ];
         cssManager.addStyle(...cssStyle);
     }
 
