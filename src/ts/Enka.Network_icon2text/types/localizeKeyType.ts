edd0b15a6bc83da8ccbd35ecc6cf584cf314c068
taketomo3141 <sapphire3141@gmail.com>
2023-09-09 05:44:02 +0000

    好感度のローカライズキーを"fren"から"FRIEND"に変更

--- src/ts/Enka.Network_icon2text/types/localizeKeyType.ts
+++ src/ts/Enka.Network_icon2text/types/localizeKeyType.ts
@@ -1,5 +1,7 @@
 import { StatsKey } from "./statsKeyType";
 
-export type LocalizeKey =
-    StatsKey |
-    "fren";
+export const LocalizeKey = {
+    ...StatsKey,
+    friend: "FRIEND",
+} as const;
+export type LocalizeKey = (typeof LocalizeKey)[keyof typeof LocalizeKey];
