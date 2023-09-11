d95f6d0d91e3c188a1a9237a2746a354cb50623d
taketomo3141 <sapphire3141@gmail.com>
2023-09-11 13:39:55 +0000

    format関数追加

---
+++ src/ts/Enka.Network_icon2text/utils/format.ts
@@ -0,0 +1,15 @@
+export function fmt(
+    template: string,
+    values?: { [key: string]: string | number | null | undefined }
+): string {
+    if (!values) return template;
+
+    // eslint-disable-next-line @typescript-eslint/no-implied-eval
+    const format = new Function(
+        ...Object.keys(values),
+        `return \`${template}\`;`
+    );
+    return format(
+        ...Object.values(values).map((value) => value ?? "")
+    ) as string;
+}
