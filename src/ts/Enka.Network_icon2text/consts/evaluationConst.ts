8f21de3db19074938a1bbbc5638869ada0fac931
taketomo3141 <sapphire3141@gmail.com>
2023-09-12 09:03:02 +0000

    聖遺物評価選択欄にヘッダーを追加 ログアウト状態で任意のアカウントのビルドカードを見ると、正しく処理されない Fixes #5

--- src/ts/Enka.Network_icon2text/consts/evaluationConst.ts
+++ src/ts/Enka.Network_icon2text/consts/evaluationConst.ts
@@ -1,6 +1,7 @@
 export namespace EvaluationConst {
     export const EVALUATION_TEXT = "evaluateText";
 
+    export const SELECTOR_HEADER = "evaluationSelectorHeader";
     export const SELECTOR_ROW = "evaluationSelectorRow";
     export const SELECTOR_DIV = "evaluationSelectorDiv";
 
